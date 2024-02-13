184d30cb89d4a543a239d643d40a31a9802e783d
Matias Reyna <matiireyna@hotmail.com>
2024-02-13 10:24:03 -0300

    Se renderiza las Cards

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -1,6 +1,23 @@
-const Cards = () => {
+import Card from './Card';
+
+const Cards = ({ characters }) => {
     return (
-        <div></div>
+        <div>
+            {
+                characters.map((carta) => {
+                    return <Card
+                        key={carta.id}
+                        name={carta.name}
+                        status={carta.status}
+                        species={carta.species}
+                        gender={carta.gender}
+                        origin={carta.origin.name}
+                        image={carta.image}
+                        onClose={() => window.alert('Emulamos que se cierra la card')}
+                    />
+                })
+            }
+        </div>
     )
 };
 
