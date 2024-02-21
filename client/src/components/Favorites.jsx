230abb596a6afd5b43795ee3fd28cdc087cd6053
Matias Reyna <matiireyna@hotmail.com>
2024-02-21 12:09:03 -0300

    Filtrado y ordenamiento: done

--- client/src/components/Favorites.jsx
+++ client/src/components/Favorites.jsx
@@ -1,6 +1,7 @@
 import { useSelector, useDispatch } from 'react-redux';
+import { useState } from 'react';
 import Card from './Card';
-import { removeFav } from '../redux/actions';
+import { removeFav, filterCards, orderCards } from '../redux/actions';
 
 const Favorites = () => {
 
@@ -8,12 +9,38 @@ const Favorites = () => {
 
     const dispatch = useDispatch();
 
+    const [ aux, setAux ] = useState(false);
+
     const handleRemoveFavorite = (id) => {
         dispatch(removeFav(id))
     };
 
+    const handleOrder = (event) => {
+        dispatch(orderCards(event.target.value))
+        setAux(!aux)
+    };
+
+    const handleFilter = (event) => {
+        if (event.target.value === 'All') {  // No se aplica ningun filtro y mostramos todos los personajes.
+            setAux(!aux)
+        } else {
+            dispatch(filterCards(event.target.value))  // Caso contrario se despacha la actions de filtrado.
+        }
+    };
+
     return (
         <div>
+            <select onChange={handleOrder}>
+                <option value='A'>ascendente</option>
+                <option value='D'>descendente</option>
+            </select>
+            <select onChange={handleFilter}>
+                <option value='All'>All</option>
+                <option value='Male'>Male</option>
+                <option value='Female'>Female</option>
+                <option value='Genderless'>Genderless</option>
+                <option value='unknown'>unknown</option>
+            </select>
             {
                 myFavorites.map((character) => {
                     return (
