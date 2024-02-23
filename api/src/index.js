bf308aadfd08a873c5fc04851f901f3ad19fb8a5
Matias Reyna <matiireyna@hotmail.com>
2024-02-23 13:06:06 -0300

    Servidor: done

---
+++ api/src/index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const characters = require('./utils/data');
+
+http.createServer((req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+
+    if (req.url.includes('/rickandmorty/character')) {
+        const id = req.url.split('/').at(-1)
+
+        let charactersFind = characters.find((character) => character.id === Number(id))
+
+        res.writeHead(200, { 'Content-type':'application/json' }).end(JSON.stringify(charactersFind))
+    }
+}).listen(3001, 'localhost')
