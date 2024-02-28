8ae8772fca74d18e0d907810c91522ad11e5e86b
Matias Reyna <matiireyna@hotmail.com>
2024-02-28 12:31:20 -0300

    Servidor levantado con express

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,7 @@
-const http = require('http');
-const { getCharById } = require('./controllers/getCharById');
+const express = require('express');
+const server = express();
+const PORT = 3001;
 
-http.createServer((req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-
-    if (req.url.includes('/rickandmorty/character')) {
-        const id = req.url.split('/').at(-1)
-
-        getCharById(res, Number(id))
-    }
-}).listen(3001, 'localhost')
+server.listen(PORT, () => {
+    console.log(`Server raised in port: ${PORT}`);
+});
