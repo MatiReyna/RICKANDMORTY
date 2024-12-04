1d487700632de826a4a99f2ca9afd36dada5631b
Mati Reyna <matiireyna@hotmail.com>
2024-12-04 19:02:16 -0300

    Manejo de error para ruta no existente

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,8 +4,12 @@ const favoriteRoutes = require('./favoriteRoutes');
 
 const router = require('express').Router();
 
-router.use('/auth', authRoutes);
+router.use('/users', authRoutes);
 router.use('/characters', characterRoutes);
 router.use('/favorites', favoriteRoutes);
+
+router.use((req, res) => {
+    res.status(404).json({ error: 'Ruta no encontrada' })
+});
 
 module.exports = router;
